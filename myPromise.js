13c7bdb3558518a3ae11070a0ea6e8b30c61d01c
jcz <xiaohuixu@126.com>
2025-06-01 14:55:41 +0800

    feat: 添加Promise实例创建和异步解析示例

--- myPromise.js
+++ myPromise.js
@@ -20,3 +20,9 @@ MyPromise.prototype.catch = function (onRejected) { // 捕获错误的方法
     }
 }
 console.log("设计模式study")
+console.log("手写Promise")
+const promise = new MyPromise((resolve, reject) => { // 创建一个新的Promise实例
+    setTimeout(() => { // 模拟异步操作
+        resolve('成功') // 调用resolve方法，将状态改为fulfilled，并存储成功的值
+    }, 1000)
+})
