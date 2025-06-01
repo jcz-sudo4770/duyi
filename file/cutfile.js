a88c736a5d281882b5869514018e3c22f8dcf939
jcz <xiaohuixu@126.com>
2025-06-01 14:57:25 +0800

    feat(文件处理): 添加大文件分片上传功能

---
+++ file/cutfile.js
@@ -0,0 +1,15 @@
+/**
+ * 大文件分片上传
+ * @param {Blob} file 
+ * @param {Number} start 
+ * @param {Number} end 
+ * @returns 
+ */
+function cutfile(file, start, end) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+    })
+
+}
+
+export default cutfile;
